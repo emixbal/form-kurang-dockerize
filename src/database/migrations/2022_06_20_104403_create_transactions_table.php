1613cfcfccfafadf66f2b79450b8877134c91c03
iqbal <emixbal@gmail.com>
2022-06-20 18:05:47 +0700

    add actor id

--- src/database/migrations/2022_06_20_104403_create_transactions_table.php
+++ src/database/migrations/2022_06_20_104403_create_transactions_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->decimal('nominal', $precision = 15, $scale = 2)->default(0);
             $table->bigInteger('transaction_type_id')->unsigned()->nullable();
             $table->foreign('transaction_type_id')->references('id')->on('transaction_types');
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('keterangan');
             $table->timestamps();
         });
