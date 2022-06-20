ec915ddf7506e4671963bfa24ac9a7efa870a53b
iqbal <emixbal@gmail.com>
2022-06-20 17:46:17 +0700

    init transaction and transaction type

---
+++ src/database/migrations/2022_06_20_104403_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('nominal', $precision = 15, $scale = 2)->default(0);
+            $table->bigInteger('transaction_type_id')->unsigned()->nullable();
+            $table->foreign('transaction_type_id')->references('id')->on('transaction_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
